40c6f73a72a1a54419e36c8b59f3d0c4248e444c
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-01-22 20:10:53 -0800

    Update Header and index.css
    This commit updates the Header component in Header.jsx by adding a "my-auto" class to the children div. It also adds a "height: fit-content" property to the .buttons class in index.css.

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -18,7 +18,7 @@ function DesktopHeader({ title, children }) {
     <div className="h-[5rem] bg-black mb-10">
       <div className="mx-page flex justify-between">
         <h1 className="text-header font-semibold">{title}</h1>
-        <div className="flex gap-5">{children}</div>
+        <div className="flex gap-5 my-auto">{children}</div>
       </div>
     </div>
   );
