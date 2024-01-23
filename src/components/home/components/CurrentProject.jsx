59e3c2a48ac9300e0bdf12ae0a93217bc13dfa97
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-01-22 20:59:08 -0800

    Add clip icon and update project description

--- src/components/home/components/CurrentProject.jsx
+++ src/components/home/components/CurrentProject.jsx
@@ -1,7 +1,22 @@
 import PropTypes from "prop-types";
 
+// icons
+import { LiaPhotoVideoSolid as ClipIcon } from "react-icons/lia";
+
 export default function CurrentProject({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      {children}
+      <ClipIcon className="w-[30%] h-auto mx-auto" />
+      <p>
+        I am a student at Oregon State University studying computer science. I
+        expect to graduate in March of 2025 with a Bachelors degree in Computer
+        Science with a focus in web and mobile application development. I am
+        currently looking for an internship for the summer of 2024. I am
+        passionate about learning new things and creating new projects.
+      </p>
+    </div>
+  );
 }
 
 CurrentProject.propTypes = {
