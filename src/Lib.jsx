b8a8cfc7ba03026e1d2acd41c1d1e63cfc6b1c65
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-01-22 20:22:50 -0800

    Refactor footer component and add OutsideLink component

--- src/Lib.jsx
+++ src/Lib.jsx
@@ -15,3 +15,16 @@ BottomBorder.propTypes = {
   children: PropTypes.node,
   click: PropTypes.func,
 };
+
+export const OutsideLink = ({ children, link }) => {
+    return (
+        <a
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+        className="buttons center w-fit flex gap-3 mt-1 hover:text-white hover:mt-0 hover:mb-1 font-semibold transition-all durration-300 ease-in-out"
+        >
+        {children}
+        </a>
+    );
+    }
--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,56 +1,32 @@
-import React from 'react'
-
 // components
-import { BottomBorder } from '../Lib'
+import { OutsideLink } from "../Lib";
 
 // icons
-import { SiGithub as GitHubLogo } from 'react-icons/si'
-import { FaLinkedinIn as LinkedLogo } from 'react-icons/fa'
-import { IoIosMail as MailLogo } from 'react-icons/io'
-
+import { SiGithub as GitHubLogo } from "react-icons/si";
+import { FaLinkedinIn as LinkedLogo } from "react-icons/fa";
+import { IoIosMail as MailLogo } from "react-icons/io";
 
 export default function Footer() {
   return (
-    <diV className='bg-footer w-full mt-20'>
-      <div className='w-fit mx-auto flex flex-col gap-4'>
-        <div className='h-16'></div>
-        <div className='flex gap-8'>
-          <BottomBorder>
-            <a
-              href='https://www.github.com/pdxgrantc'
-              target='_blank'
-              rel='noreferrer'
-              className='my-auto w-fit mt-1 hover:mt-0 hover:mb-1 transition-all durration-300 ease-in-out'
-            >
-              <GitHubLogo className='h-[4rem] w-auto py-2 mx--auto text-button hover:text-button_hover' />
-            </a>
-          </BottomBorder>
-          <BottomBorder>
-            <a
-              href='mailto:pdxgrantc@gmail.com'
-              target='_blank'
-              rel='noreferrer'
-              className='mt-1 hover:mt-0 hover:mb-1 transition-all durration-300 ease-in-out'
-            >
-              <MailLogo className='h-[4rem] py-0 w-auto text-button hover:text-button_hover' />
-            </a>
-          </BottomBorder>
-          <BottomBorder>
-            <a
-              href='https://www.github.com/pdxgrantc'
-              target='_blank'
-              rel='noreferrer'
-              className='w-auto mt-1 hover:mt-0 hover:mb-1 transition-all durration-300 ease-in-out'
-            >
-              <LinkedLogo className='h-[3.5rem] w-auto text-button hover:text-button_hover' />
-            </a>
-          </BottomBorder>
+    <diV className="bg-footer w-full mt-20">
+      <div className="w-fit mx-auto flex flex-col gap-4">
+        <div className="h-16"></div>
+        <div className="flex gap-8">
+          <OutsideLink link="https://www.github.com/pdxgrantc">
+            <GitHubLogo className="h-[4rem] w-auto py-2 mx--auto text-button hover:text-button_hover" />
+          </OutsideLink>
+          <OutsideLink link="mailto:pdxgrantc@gmail.com">
+            <MailLogo className="h-[4rem] py-0 w-auto text-button hover:text-button_hover" />
+          </OutsideLink>
+          <OutsideLink link="https://www.linkedin.com/in/pdxgrantc">
+            <LinkedLogo className="h-[3.5rem] w-auto text-button hover:text-button_hover" />
+          </OutsideLink>
         </div>
-        <div className='mx-auto w-fit'>
-          <p className='w-fit'>Grant Conklin @2024</p>
+        <div className="mx-auto w-fit">
+          <p className="w-fit">Grant Conklin @2024</p>
         </div>
-        <div className='h-16'></div>
+        <div className="h-16"></div>
       </div>
     </diV>
-  )
+  );
 }
