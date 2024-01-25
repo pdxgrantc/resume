fd06c3c6eac70bb9863c5fbd96ccb68e86ad5cef
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-01-25 13:20:26 -0800

    Refactor Projects component to use separate components for normal and reverse layout

--- src/components/home/components/Projects.jsx
+++ src/components/home/components/Projects.jsx
@@ -7,8 +7,13 @@ export default function Projects({ children }) {
   return (
     <div>
       {children}
-      <div className="flex gap-20 my-5">
-        <Project
+      <div className="flex flex-col gap-20 my-5">
+        <ProjectNorm
+          name={"Recipe Storage"}
+          link={"https://budget.pdxgrantc.com"}
+          imgName={"Budget.jpg"}
+        />
+        <ProjectRev
           name={"Recipe Storage"}
           link={"https://budget.pdxgrantc.com"}
           imgName={"Budget.jpg"}
@@ -22,7 +27,7 @@ Projects.propTypes = {
   children: PropTypes.node,
 };
 
-function Project({ name, link, imgName }) {
+function ProjectNorm({ name, link, imgName }) {
   const [url, setUrl] = useState(null);
 
   // Get home page image url from firebase storage
@@ -36,9 +41,43 @@ function Project({ name, link, imgName }) {
   });
 
   return (
-    <div className="w-full">
-      <h4 className="">{name}</h4>
-      <a className="relative" href={link} target="_blank" rel="noreferrer">
+    <div className="w-full grid grid-cols-7 gap-8">
+      <a className="relative col-span-5" href={link} target="_blank" rel="noreferrer">
+        <div className="absolute w-full h-full bg-accent_red z-0 top-2 left-2"></div>
+        <img src={url} alt={name} className="relative w-full h-full z-1" />
+      </a>
+      <div className="col-span-2">
+        <h4 className="">{name}</h4>
+      </div>
+    </div>
+  );
+}
+
+ProjectNorm.propTypes = {
+  name: PropTypes.string,
+  link: PropTypes.string,
+  imgName: PropTypes.string,
+};
+
+function ProjectRev({ name, link, imgName }) {
+  const [url, setUrl] = useState(null);
+
+  // Get home page image url from firebase storage
+  const getURL = async () => {
+    const storageRef = ref(storage, imgName);
+    const url = await getDownloadURL(storageRef);
+    setUrl(url);
+  };
+  useEffect(() => {
+    getURL();
+  });
+
+  return (
+    <div className="w-full grid grid-cols-7 gap-8">
+      <div className="col-span-2">
+        <h4 className="">{name}</h4>
+      </div>
+      <a className="relative col-span-5" href={link} target="_blank" rel="noreferrer">
         <div className="absolute w-full h-full bg-accent_red z-0 top-2 left-2"></div>
         <img src={url} alt={name} className="relative w-full h-full z-1" />
       </a>
@@ -46,7 +85,7 @@ function Project({ name, link, imgName }) {
   );
 }
 
-Project.propTypes = {
+ProjectRev.propTypes = {
   name: PropTypes.string,
   link: PropTypes.string,
   imgName: PropTypes.string,
